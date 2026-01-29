40d9386e13d884d0a3c738f0d185ed7f35addcb9
JordanLaw <123150515+JordanLaw@users.noreply.github.com>
2026-01-29 21:18:01 +0800

    Update index.ts

--- lab3/index.ts
+++ lab3/index.ts
@@ -1,1 +1,115 @@
 
+import Koa from "koa";
+import Router, {RouterContext} from "koa-router";
+import logger from "koa-logger";
+import json from "koa-json";
+import bodyParser from "koa-bodyparser";
+import { CustomErrorMessageFunction, query, body, validationResults } from "koa-req-validation";
+
+const app: Koa = new Koa();
+const router: Router = new Router();
+
+const customErrorMessage: CustomErrorMessageFunction = (
+        _ctx: RouterContext,
+        value: string
+    ) => {
+        return (
+            `The name must be between 3 and 20 ` +
+            `characters long but received length ${value.length}`
+        );
+};
+
+const validatorName = [
+    body("name").isLength({ min: 3 }).withMessage(customErrorMessage).build(),
+    // body("id").isInt({ min: 10000, max: 20000 }).build()
+]
+
+const film = [
+        {
+            id: 1,
+            title: "Star War I",
+            year: "1980"
+        },
+        {
+            id: 2,
+            title: "Star War II",
+            year: "1985"
+        },
+        {
+            id: 3,
+            title: "Star War III",
+            year: "1990"
+        }
+    ];
+
+// router.get('/', async (ctx: RouterContext, next: any) => {
+router.get('/', ...validatorName, async (ctx: RouterContext, next: any) => {
+    const result = validationResults(ctx);
+    if( result.hasErrors() ){
+        ctx.status = 422;
+        ctx.body = { err: result.mapped() }
+    }else{
+        const name = ctx.request.body.name;
+        ctx.body = { msg: `Hello world! ${name}` };
+    }
+    await next();
+})
+
+router.get('/film', async (ctx: RouterContext, next: any) => {
+    ctx.body = film;
+    await next();
+})
+
+router.get('/film/:id', async (ctx: RouterContext, next: any) => {
+    const id = ctx.params.id;
+    film.forEach((f,i) => {
+        if (id == f.id){
+            ctx.body = film[i];
+        }
+    });
+})
+
+router.post('/film', async (ctx: RouterContext, next: any) => {
+    const data = ctx.request.body;
+    film.push(data);
+    ctx.body = data
+    await next();
+})
+
+router.put('/film', async (ctx: RouterContext, next: any) => {
+    const data = ctx.request.body;
+    film.forEach((f) => {
+        if (data.id == f.id) {
+            f.title = data.title;
+            f.year = data.year;
+        }
+    })
+    ctx.body = film;
+    await next();
+})
+
+router.post('/', async (ctx: RouterContext, next: any) => {
+    const data = ctx.request.body;
+    ctx.body = data;
+    await next();
+})
+
+app.use(json());
+app.use(logger());
+app.use(bodyParser());
+app.use(router.routes()).use(router.allowedMethods());
+app.use(async (ctx: RouterContext, next: any) => {
+    try {
+        await next();
+        if (ctx.status === 404) {
+            ctx.status = 404;
+            ctx.body = { err: "No such endpoint existed" };
+        }
+    } catch (err: any) {
+        ctx.body = { err: err };
+    }
+})
+
+app.listen(10888, () => {
+    console.log("Koa Started");
+})
